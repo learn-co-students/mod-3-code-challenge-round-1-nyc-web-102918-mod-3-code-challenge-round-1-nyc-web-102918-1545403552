57fd4b1abde24f7b597fced93c104eb6948328f8
ruthmesfun <ruth.mesfun@gmail.com>
2018-12-21 09:58:21 -0500

    half way

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 document.addEventListener('DOMContentLoaded', () => {
   console.log('%c DOM successfully loaded and parsed!', 'color: firebrick')
+
+  const author = document.getElementById('author-select')
+
+  author.addEventListener('change', (event) => {
+    let author_id = event.target.value
+
+    fetch(`http://localhost:3000/authors/${author_id}?_embed=blogPosts`)
+    .then(r => r.json())
+    .then(info => {
+      const authorContainer = document.getElementById('author-container')
+      console.log(info)
+    })
+  })
 })
